8a90e2373bbcff959df974bc2397131dc5a18bb1
ygorxds <ygordesa2010@hotmail.com>
2023-06-21 15:35:09 -0300

    update: produto

--- src/controllers/produtos.controller.js
+++ src/controllers/produtos.controller.js
@@ -4,14 +4,14 @@ const knex = createKnex();
 
 const ProdutoController = {
     async buscar(req, res) {
-        let clientes = await knex('CLIENTE').select('*');
+        let clientes = await knex('PRODUTO').select('*');
         return res.json(clientes);
     },
 
    
     async inserirCliente(req, res) {
         const cliente = req.body;
-        let result = await knex('CLIENTE').insert(cliente);
+        let result = await knex('PRODUTO').insert(cliente);
         if(!result) return res.status(400).json({msg:'user does not inserted'});
         return res.status(200).json({msg:'user inserted'});
 
